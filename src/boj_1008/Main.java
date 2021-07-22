40be7a78e01e848e2caaa17ebee5b5db41c7eb20
hskoo <nrudev812@gmail.com>
2021-07-23 08:34:43 +0900

    1008. A/B

---
+++ src/boj_1008/Main.java
@@ -0,0 +1,13 @@
+package boj_1008;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        System.out.println((double) A / B);
+    }
+}
