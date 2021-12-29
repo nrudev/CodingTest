508378f89fc4dd002959d8844766ce182b317bfe
hskoo <nrudev812@gmail.com>
2021-12-29 15:29:27 +0900

    10871. X보다 작은 수

---
+++ src/boj_10871/Main.java
@@ -0,0 +1,27 @@
+package boj_10871;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+
+            if (num < X) {
+                bw.write(num + " ");
+            }
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
